45ebc33a182eabda5f5e32f66b6fbd3a3bf0a5a2
Daria Samodelova <sam-dash@yandex.ru>
2024-07-01 20:42:42 +0300

    git init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header";
+import Main from './pages/Main/Main';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <div className="container"><Main /></div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import NewsBanner from '../../components/NewsBanner/NewsBanner'
+import styles from './Main.module.css'
+import { getNews } from '../../api/apiNews'
+import NewsList from '../../components/NewsList/NewsList'
+
+const Main = () => {
+    const [news, setNews] = useState([])
+
+    const fetchNews = async () => {
+        try {
+            const response = await getNews()
+            setNews(response.news);
+            console.log(response.news);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchNews()
+    }, [])
+
+    return (
+        <main className={styles.main}>
+            {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
+            <NewsList news={news} />
+        </main>
+    )
+}
+
+export default Main
